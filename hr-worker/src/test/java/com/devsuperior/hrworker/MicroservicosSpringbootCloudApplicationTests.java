10fe1be7ed46b50b2b45413c8260a45bab50936c
Gleides Vinente <gleidevelop@gmail.com>
2022-04-04 15:31:25 -0400

    init project

---
+++ hr-worker/src/test/java/com/devsuperior/hrworker/MicroservicosSpringbootCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.hrworker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicosSpringbootCloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
